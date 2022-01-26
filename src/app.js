da7a4231e49a82cb80bf2173488908f6c6becac1
masembe14 <masembeyolan955@gmail.com>
2022-01-26 07:17:19 +0300

    Added Url Encoder, Json and rest Express middleware to app

--- src/app.js
+++ src/app.js
@@ -4,9 +4,17 @@ const app = express(feathers());
 const services = require('./services')
 const apphooks =require('./app.hooks')
 
+
+// Turn on JSON parser for REST services
+app.use(express.json())
+// Turn on URL-encoded parser for REST services
+app.use(express.urlencoded({ extended: true }));
+// Set up REST transport
+app.configure(express.rest())
+
 app.configure(services)
 
-app.use(express.notFound());
+//app.use(express.notFound());
 
 
 
